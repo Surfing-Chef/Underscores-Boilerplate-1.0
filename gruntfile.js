059b733dfba2cb37d8b395d774933b080af0cbdf
Surfing-Chef <surfing-chef@surfing-chef.com>
2016-12-31 15:44:22 -0800

    Modified gruntfile.js
    Added watch plugin, and watch task.
    Registered the watch task as default

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,10 @@
 // Wrapper function
 module.exports = function(grunt) {
+
   // Load Tasks
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
   // Project configuration
   grunt.initConfig({
     // Tasks to run, comma separated
@@ -13,6 +16,14 @@ module.exports = function(grunt) {
           'js/skip-link-focus-fix.js': ['dev/js/skip-link-focus-fix.js']
         } // files
       } // my_target
-    } // uglify
+    }, // uglify
+    watch: {
+      files: ['dev/js/*.js'],
+      tasks: ['uglify']
+    } // watch
   }); // grunt.initConfig
+
+  // Register default task(s)
+  grunt.registerTask('default', 'watch');
+
 }; // module.exports
