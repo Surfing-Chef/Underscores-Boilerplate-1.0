3dc39b7d22103d860697105667b1210bc55a37ba
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-01-11 16:51:26 -0800

    Created a package.json

---
+++ config_prod.rb
@@ -0,0 +1,12 @@
+# DEPLOYMENT CONFIGURATION
+require 'susy'
+
+# COMPASS CONFIGURATION
+project_type = :stand_alone
+http_path = "/"
+css_dir = "."
+sass_dir = "_/sass"
+line_comments = false
+preferred_syntax = :scss
+output_style = :compressed
+relative_assets = true
