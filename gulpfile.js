c6e1746369f7479a7a781add7e468fb00edec127
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-01-13 07:31:07 -0800

    added php, html, and browserSync tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,27 @@
 // REQUIRED
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
-    compass = require('gulp-compass');
-    plumber = require('gulp-plumber');
-    autoprefixer = require('gulp-autoprefixer');
+    compass = require('gulp-compass'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    php = require('gulp-connect-php'),
+    plumber = require('gulp-plumber'),
+    autoprefixer = require('gulp-autoprefixer'),
     rename = require('gulp-rename');
 
 // NAMED TASKS
-// Scripts Task
-gulp.task('scripts', function(){  // task called 'scripts'
+// Scripts Task - tasks related to js
+gulp.task('scripts', function(){
   gulp.src(['dev/js/**/*.js', '!dev/js/**/*.min.js'])
   .pipe(plumber())
   .pipe(rename({suffix:'.min'}))
   .pipe(uglify())
-  .pipe(gulp.dest('dev/js'));
+  .pipe(gulp.dest('dev/js'))
+  .pipe(reload({stream: true}));
 
 });
 
-// Compass/Sass Tasks - watch files and folders for changes
+// Compass/Sass Tasks - tasks related to sc scss and css
 gulp.task('compass', function(){
   gulp.src('dev/sass/**/*.scss')
   .pipe(plumber())
@@ -27,14 +31,38 @@ gulp.task('compass', function(){
     sass: 'dev/sass'
   }))
     .pipe(autoprefixer('last 2 versions'))
-    .pipe(gulp.dest('dev/css/'));
+    .pipe(gulp.dest('dev/css/'))
+    .pipe(reload({stream: true}));
+});
+
+// PHP Tasks - tasks related to php
+gulp.task('php', function(){
+  php.server({
+    base: './',
+    port: 810,
+    keepalive: true
+  });
+});
+
+// HTML Tasks - tasks related to html
+gulp.task('html', function(){
+  gulp.src('./')
+  .pipe(reload({stream: true}));
+});
+
+// Browser-Sync Tasks - tasks related to browser-sync
+gulp.task('browser-sync', function(){
+  browserSync({
+    proxy: 'http://localhost/underscores-dev/'
+  });
 });
 
 // Watch Task - watch files and folders for changes
 gulp.task('watch', function(){
   gulp.watch('dev/js/**/*.js', ['scripts']);
   gulp.watch('dev/sass/**/*.scss', ['compass']);
+  gulp.watch('**/*.php', ['php']);
 });
 
 // Default Task - runs specified tasks asynchronously
-gulp.task('default', ['scripts', 'compass', 'watch']);
+gulp.task('default', ['scripts', 'compass', 'html', 'php', 'browser-sync', 'watch']);
